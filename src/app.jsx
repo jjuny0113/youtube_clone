59049fe87cd729e259efe243bb99e20d75c1393a
jjuny0113 <jjuny920113@gmail.com>
2020-11-09 00:28:06 -0800

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import "./app.css";
-
+import VideoList from "./components/video_list/video_list";
 function App() {
-  return <h1>Hello ^^</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCX3EL_Eu5tQ_0oRIt7Flx7GaliBJn_uHo",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  useEffect(() => {});
+  return <VideoList videos={videos} />;
 }
 
 export default App;
