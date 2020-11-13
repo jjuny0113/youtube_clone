1edfa65878e044c195211b2e3654381f090b6492
jjuny0113 <jjuny920113@gmail.com>
2020-11-12 23:31:18 -0800

    add youtube detail

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoItem from "./components/video_item/video_item";
 import VideoList from "./components/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const search = (query) => {
+    setSelectedVideo(null);
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+      });
   };
 
   useEffect(() => {
@@ -21,7 +31,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -19,7 +19,7 @@ class Youtube {
   async search(query) {
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.requestOptions
+      this.getRequestOptions
     );
     const result = await response.json();
     return result.items.map((item) => ({ ...item, id: item.id.videoId }));
